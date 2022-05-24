cce23b3d988370b1266bb0ad5e52e8c7b53eb379
iitenkida7 <t.ishii@teradox.net>
2022-05-24 09:36:24 +0900

    Add app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import ytch  from 'yt-channel-info'
 import * as http from 'http';
 
+import url from  require('url');
+//var http = require('http');
+
+
 
 console.log('Up http server.')
 http
   .createServer((req, res) => {
     if (req.method === 'GET') {
+
+      var url_parse = url.parse(req.url, true);
+      console.log(url_parse);
+
+
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Hello world\n');
     }
@@ -13,8 +22,8 @@ http
   .listen(32768);
 
 
-/* 
 
+  /*
 const payload = {
   channelId: 'takaponjp', // Required
   channelIdType: 0,
